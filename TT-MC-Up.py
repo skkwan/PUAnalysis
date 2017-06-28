7c859aad5dd231a45f4b3631a2b636b8e59d866c
isobelojalvo <ojalvo@wisc.edu>
2017-06-28 08:48:06 -0500

    adding in separate files for upgrades studies

---
+++ TT-MC-Up.py
@@ -0,0 +1,105 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("ANALYSIS")
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+
+process.GlobalTag.globaltag = '91X_upgrade2023_realistic_v2'
+
+process.options   = cms.untracked.PSet(wantSummary = cms.untracked.bool(False))
+process.options.allowUnscheduled = cms.untracked.bool(True)
+
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1000)
+)
+
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 10
+
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        'file:/cms/ojalvo/Timing-Htt/VBF-Htt-PU200-MINI.root'
+		),
+		inputCommands=cms.untracked.vstring(
+						'keep *',
+						'keep *_l1extraParticles_*_*',
+		)
+)
+
+
+#Default Reconstruction from the analysTools.py config file
+#The main 'setup' processes can be found in "defaultReconstruction" and "defaultReconstructionMC"
+#analysisTools takes care of pre-selections, adding in extra ID's that do not come in the miniAOD
+#files and embedding trigger matching (the trigger for di-Tau hadronic still should be added and checked)
+#defaultRecontruction and defaultReconstructionMC are generic enough to work for any final state
+#needed: muTau, tauTau, eTau, mumuTauTau, ect. However, different analyses may want to embed
+#different ID's, isolations, ect. Trigger paths are input below. These plugins are typically
+#found in RecoTools/plugins/
+from PUAnalysis.Configuration.tools.analysisToolsUpgrade import *
+defaultReconstructionMC(process,'HLT2',
+        [
+        'HLT_DoubleMediumIsoPFTau35_Trk1_eta2p1_Reg_v2'
+        ])
+
+#EventSelection
+#Most of the selections for the analysis go in hTauTau_cff
+#The selections proceed sequentially, each time a "di-candidate pair" fails a cut in
+#this configuration then the sequence will start over with another di-candidate pair.
+#The final 'sorting' is implemented there as well, either by di-tau PT or isolation.
+process.load("PUAnalysis.Configuration.hTauTauUpgrade_cff")
+
+process.metCalibration.applyCalibration = cms.bool(False)
+
+#Name of the path in hTauTau_cff
+process.eventSelectionTT = cms.Path(process.selectionSequenceTT)
+
+#Specifies which gen particles we wish to keep, this collection is used in the
+#RecoTools/interface/CompositePtrCandidateT1T2MEtAlgorithm.h
+#Most of the algorithm tools are done in the composite ptr candidate algorithm 
+#module. 
+createGeneratedParticles(process,
+        'genDaughters',
+        [
+            "keep++ pdgId = {Z0}",
+            "keep pdgId = {tau+}",
+            "keep pdgId = {tau-}",
+            "keep pdgId = {mu+}",
+            "keep pdgId = {mu-}",
+            "keep pdgId = 6",
+            "keep pdgId = -6",
+            "keep pdgId = 11",
+            "keep pdgId = -11",
+            "keep pdgId = 25",
+            "keep pdgId = 35",
+            "keep pdgId = 37",
+            "keep pdgId = 36"
+            ]
+        )
+
+#TBH, I am not sure if this is currently used.
+createGeneratedParticles(process,
+        'genTauCands',
+        [
+            "keep pdgId = {tau+} & mother.pdgId()= {Z0}",
+            "keep pdgId = {tau-} & mother.pdgId() = {Z0}"
+            ]
+        )
+
+#Create the Ntuples, the name "analysis.root" is set here as well
+#This takes the output from the configuration sequence and fills
+#the tree that will later be used for plotting. Notice the names
+#added here 'diTauOS' is the default, however, this 'diTauOS' can
+#be replaced with any of the labled collections produced in htautau_cff.py
+#in order to create two different trees, one with all the final selections
+#and one with looser selections.
+from PUAnalysis.Configuration.tools.ntupleToolsUpgrade import addDiTauEventTree
+addDiTauEventTree(process,'diTauEventTree')
+addDiTauEventTree(process,'diTauEventTreeFinal','diTausOS')
+
+#This event summary tells you how many objects pass each of the steps
+#in the configuration. It is extremely useful for debugging. 
+#Normally does not need to be touched. :) 
+addEventSummary(process,True,'TT','eventSelectionTT')
+
